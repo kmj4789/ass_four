09094d8134a5d67e946c8c9c5093dbbbb58ae47f
kmj4789 <81201559+kmj4789@users.noreply.github.com>
2022-04-07 04:32:25 +1200

    adding dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ const App = () =>  {
             <Route  path="/SEPractice" component={SEPractice}/>
             <Route  path="/SubmitArticle" component={SubmitArticle}/>
             <Route exact path="/404" component={NotFoundPage}/>
-            <Redirect to="/404" />
+            
           </div>
         </div>
         </Router>
